4a14e42f75dc88037d1785408282266c185282af
sikoob <simon.koob@gmx.net>
2023-09-24 14:32:54 +0200

    working on navbar setup

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import justpy as jp
 from webapp.about import About
+from webapp.dictionary import Dictionary
 from webapp.home import Home
 
 jp.Route(Home.path, Home.serve)
 jp.Route(About.path, About.serve)
+jp.Route(Dictionary.path, Dictionary.serve)
 
 jp.justpy()
---
+++ webapp/dictionary.py
@@ -0,0 +1,30 @@
+import justpy as jp
+import definition
+
+
+class Dictionary:
+    path = "/dictionary"
+
+    @classmethod
+    def serve(cls, req):
+        wp = jp.QuasarPage(tailwind=True)
+        div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+        jp.Div(a=div, text="Instant English Dictionary", classes="text-4xl m-2")
+        jp.Div(a=div, text="Get the definition of any English word instantly as you type.", classses="text-lg")
+
+        input_div = jp.Div(a=div, classes="grid grid-cols-2")
+
+        output_div = jp.Div(a=div, classes="m-2 p-2 text-lg border-2 h-40")
+
+        input_box = jp.Input(a=input_div, placeholder="Type in a word you search here", outputdiv=output_div,
+                             classes="m-2 bg-gray-100 border-2 border-gray-200 rounded w-64 "
+                                     "focus:outline-none focus: bg-white focus: border-purple-500 py-2 px-4")
+
+        input_box.on('input', cls.get_definition)
+
+        return wp
+
+    @staticmethod
+    def get_definition(widget, msg):
+        defined = definition.Definition(widget.inputbox.value).get()
+        widget.outputdiv.text = "\n\n".join(defined)
