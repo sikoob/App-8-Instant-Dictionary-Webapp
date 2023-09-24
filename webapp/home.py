2223f3aeabfa7f50f525b80caa17471885c64ee8
sikoob <simon.koob@gmx.net>
2023-09-24 15:15:21 +0200

    making navbar available on all sites

--- webapp/home.py
+++ webapp/home.py
@@ -1,4 +1,5 @@
 import justpy as jp
+from webapp import layout
 
 
 class Home:
@@ -8,27 +9,9 @@ class Home:
     def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
 
-        layout = jp.QLayout(a=wp, view="hHh lpR fFf")
-        header = jp.QHeader(a=layout)
-        toolbar = jp.QToolbar(a=header, )
+        lay = layout.DefaultLayout(a=wp)
 
-        drawer = jp.QDrawer(a=layout, show_if_above=True, v_model="left", bordered=True)
-
-        scroller = jp.QScrollArea(a=drawer, classes="fit")
-        qlist = jp.QList(a=scroller)
-        a_classes = "p-2 m-2 text-lg text-blue-400 hover:text-blue-700"
-        jp.A(a=qlist, text="Home", href="/", classes=a_classes)
-        jp.Br(a=qlist)
-        jp.A(a=qlist, text="Dictionary", href="/dictionary", classes=a_classes)
-        jp.Br(a=qlist)
-        jp.A(a=qlist, text="About", href="/about", classes=a_classes)
-        jp.Br(a=qlist)
-
-        jp.QBtn(a=toolbar, dense=True, flat=True, round=True, icon="menu", click=cls.move_drawer, drawer=drawer)
-        jp.QToolbarTitle(a=toolbar, text="Instant Dictionary")
-
-        container = jp.QPageContainer(a=layout)
-
+        container = jp.QPageContainer(a=lay)
         div = jp.Div(a=container, classes="bg-gray-200 h-screen p-2")
         jp.Div(a=div, text="This is the Home page!", classes="text-4xl m-2")
         jp.Div(a=div, text="""
@@ -42,12 +25,6 @@ class Home:
                 """, classes="text-lg")
         return wp
 
-    @staticmethod
-    def move_drawer(widget, msg):
-        if widget.drawer.value:
-            widget.drawer.value = False
-        else:
-            widget.drawer.value = True
 
 
 
